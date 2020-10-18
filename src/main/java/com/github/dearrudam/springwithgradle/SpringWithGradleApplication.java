c624b11c918ba0405a0a38fc032f7e974eb2e2b3
Maximillian Arruda <dearrudam@gmail.com>
2020-10-18 01:01:54 -0300

    first commit

---
+++ src/main/java/com/github/dearrudam/springwithgradle/SpringWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.github.dearrudam.springwithgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dearrudam/springwithgradle/SpringWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dearrudam.springwithgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
